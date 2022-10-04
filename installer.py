a66334ccfbd11129dc279642975d77db57b2d1a1
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 20:57:49 +0000

    Adding numeric_owner as keyword arguement

--- installer.py
+++ installer.py
@@ -45,7 +45,7 @@ def decompress(source):
                     if not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
             
-                tar.extractall(path, members, numeric_owner) 
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
                 
             
             safe_extract(tar, path=INSTALL_FOLDER)
